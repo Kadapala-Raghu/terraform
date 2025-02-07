f78ea5b72aade215f16889d40638da3846610eaf
Raghu <kadapalaraghu005@gmail.com>
2025-02-07 17:07:09 +0530

    teraform

---
+++ ec2/ec2.tf
@@ -0,0 +1,28 @@
+resource "aws_instance" "this" {
+  ami                    = "ami-09c813fb71547fc4f" ## This AMI joindevops 
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  instance_type          = "t2.micro"
+
+  tags = {
+    name    = "terraform-demo"
+    Purpose = "terraform practice"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "all-tls"
+  description = "Allow TLS inbound traffic and outbound traffic"
+
+  ingress {         ## Here ingress means INCOMING traffic
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    
+  egress  {     ## OUTGOING traffic
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1" # semantically equivalent to all
+  }
+}
