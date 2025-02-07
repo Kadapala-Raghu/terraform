9bb04cdec451a6c49c940086220b351266c95a9c
Raghu <kadapalaraghu005@gmail.com>
2025-02-07 18:48:04 +0530

    teraform

---
+++ count/variables.tf
@@ -0,0 +1,14 @@
+variable "instances" {
+  
+  default = ["mysql", "backend", "frontend"]
+}
+
+variable "zone_id" {
+  
+  default = "Z0720608U79R1SW06Z12"
+}
+
+variable "domain_name" {
+    default = "devopsyugam.site"
+  
+}
