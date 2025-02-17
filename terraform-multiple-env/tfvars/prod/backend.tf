8b877b19bb58d85c53a41fc52251c4586ceef8f8
Raghu <kadapalaraghu005@gmail.com>
2025-02-17 23:33:52 +0530

    terraform

--- terraform-multiple-env/tfvars/prod/backend.tf
+++ terraform-multiple-env/tfvars/prod/backend.tf
@@ -1,5 +1,4 @@
   bucket = "82s-devopsyugam-state-prod"
   key = "ec2-demo-backend-infra-prod"  # It should be unique within the bucket and same key not recomended to use another bucket
   dynamodb_table = "82s-devopsyugam-state-prod"
-  region = "us-east-1"  
-
+  region = "us-east-1"
